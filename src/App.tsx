0e7e2673ea70d5eaa54e8a9819185af343e9afd9
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-06-01 14:05:28 +0530

    Installed required packages and setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="text-blue-500 grid place-items-center">Hello World</h3>
     </div>
   );
 }
